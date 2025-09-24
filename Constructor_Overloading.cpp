9f2972f381a23428eecff45a325431a0c397d9be
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-09-24 15:51:21 +0530

    Update Constructor_Overloading.cpp

--- Constructor_Overloading.cpp
+++ Constructor_Overloading.cpp
@@ -1,73 +1,38 @@
-/*
-Name : Dhruv Jain
-PRN : 24070123042
-Batch : A2
-*/
+// Name: Dhruv Jain
+// PRN: 24070123042
+// Batch: EnTC A2
+
 #include <iostream>
 using namespace std;
-class Student{
-    int roll_no;
-    string name;
-    char div;
-    public:
-    Student(int r=0,string n="xyz",char d='X'){      // default constuctor and parameterized constructor
-        roll_no=r;
-        name=n;
-        div=d;
+
+class Rectangle {
+public:
+    // No-argument constructor
+    Rectangle() {
+        cout << "Rectangle not defined yet!" << endl;
     }
-    Student(Student &s1){           // copy constructor
-        roll_no=s1.roll_no;
-        name=s1.name;
-        div=s1.div;
+
+    // Constructor with one argument (Square)
+    Rectangle(int side) {
+        cout << "Area of Square with side " << side << " is " << side * side << endl;
     }
-    void display(){
-        cout << "Student Name: " << name << endl;
-        cout << "Student Roll No. : " << roll_no << endl;
-        cout << "Student Division : " << div << endl;
+
+    // Constructor with two arguments (Rectangle)
+    Rectangle(int length, int breadth) {
+        cout << "Area of Rectangle (" << length << " x " << breadth << ") is " << length * breadth << endl;
     }
 };
-int main(){
-    //Syntax for Student Class Constuctor:- roll_no.,Name,Div
-    int roll;string s;char c;
-    Student s1;
-    Student s2(42);
-    Student s3(42,"Dhruv");
-    Student s4(42,"Dhruv",'A');
-    cout << "Type roll_no., Name, Div : ";
-    cin >> roll >> s >> c;
-    Student s5(roll,s,c);
-    cout << "Default Constructor :" << endl;
-    s1.display();
-    cout << endl;
-    cout << "s2.display() :"<<endl;
-    s2.display();
-    cout << endl;
-    cout << "s3.display :" << endl;
-    s3.display();
-    cout << endl;
-    cout << "User input displayed : " << endl;
-    s5.display();
+
+int main() {
+    Rectangle r1;             // Calls no-argument constructor
+    Rectangle square(5);      // Calls single-argument constructor
+    Rectangle rect(10, 20);   // Calls two-argument constructor
+    return 0;
 }
-/* 
-Output:
-Type roll_no., Name, Div : 42 Dhruv A
-Default Constructor :
-Student Name: xyz
-Student Roll No. : 0
-Student Division : X
 
-s2.display() :
-Student Name: xyz
-Student Roll No. : 42
-Student Division : X
-
-s3.display :
-Student Name: Dhruv
-Student Roll No. : 42
-Student Division : X
-
-User input displayed :
-Student Name: Dhruv
-Student Roll No. : 42
-Student Division : A
+/*
+OUTPUT ::
+Rectangle not defined yet!
+Area of Square with side 5 is 25
+Area of Rectangle (10 x 20) is 200
 */
